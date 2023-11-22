cc2a90d17cc88fc2cf30ab2f4db2024b19565553
StefanoDalMas <stefano.dalmas01@gmail.com>
2023-11-22 17:06:52 +0100

    constants refactor

--- src/route_generator.py
+++ src/route_generator.py
@@ -1,52 +1,28 @@
 # system imports
-import argparse
 import random
 import json
 from typing import List, Dict, Tuple
 
 # custom imports
 from tools import cities_products as cp
+from tools.parameters import Parameters as p
 
 
-def config() -> Tuple[int, int, int, int, int, str]:
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-e",
-        "--entries",
-        type=int,
-        help="How many entries are going to be generated",
-        default=100,
-    )
-    parser.add_argument(
-        "-mint", "--mintrip", type=int, help="Minimum number of route", default=1
-    )
-    parser.add_argument(
-        "-maxt", "--maxtrip", type=int, help="Maximum number of route", default=3
-    )
-    parser.add_argument(
-        "-minp", "--minproducts", type=int, help="Minimum number of products", default=1
-    )
-    parser.add_argument(
-        "-maxp", "--maxproducts", type=int, help="Maximum number of products", default=3
-    )
-    parser.add_argument(
-        "-fn",
-        "--filename",
-        type=str,
-        help="Filename to save the data",
-        default="standard",
-    )
-    args = parser.parse_args()
-    return (
-        args.entries,
-        args.mintrip,
-        args.maxtrip,
-        args.minproducts,
-        args.maxproducts,
-        args.filename + ".json",
-    )
 
-def standardRouteGenerator(entries:int, minTrip:int, maxTrip:int, minProducts:int, maxProducts:int, fileName:int):
+
+def load_config() -> Tuple[int, int, int, int, int, str]:
+    ENTRIES = int(p.ENTRIES)
+    MINTRIP = int(p.MINTRIP)
+    MAXTRIP = int(p.MAXTRIP)
+    MINPRODUCTS = int(p.MINPRODUCTS)
+    MAXPRODUCTS = int(p.MAXPRODUCTS)
+    FILENAME = str(p.FILENAME)
+    return ENTRIES, MINTRIP, MAXTRIP, MINPRODUCTS, MAXPRODUCTS, FILENAME
+
+
+
+
+def standardRouteGenerator(entries:int, minTrip:int, maxTrip:int, minProducts:int, maxProducts:int, fileName:str):
     # open "data/standard.json, if it's not there, create it"
     with open("data/" + fileName, "w") as f:
         json_array = []
@@ -71,10 +47,6 @@ def standardRouteGenerator(entries:int, minTrip:int, maxTrip:int, minProducts:in
                 length = 0
                 for e in route:
                     length += 1
-                for r in route:
-                    print(r, "\n")
-                print("len is ", length, "\n")
-                print("length of route: " + str(len(route)))
                 # print length of route
 
             json_data.update({"id": "s" + str(entry)})
@@ -85,7 +57,8 @@ def standardRouteGenerator(entries:int, minTrip:int, maxTrip:int, minProducts:in
 
 
 if __name__ == "__main__":
-    ENTRIES, MINTRIP, MAXTRIP, MINPRODUCTS, MAXPRODUCTS, FILENAME = config()
+    ENTRIES, MINTRIP, MAXTRIP, MINPRODUCTS, MAXPRODUCTS, FILENAME = load_config()
+
     if (MINTRIP > MAXTRIP or MINPRODUCTS > MAXPRODUCTS):
         exit("wrong argument: make sure to pass correct values for min and max arguments")
     standardRouteGenerator(ENTRIES, MINTRIP, MAXTRIP, MINPRODUCTS, MAXPRODUCTS, FILENAME)
