a70dadf2247c8296436876dca791fcbaf15210aa
Stefano Sacchet <stefanosacchet@MacBook-Pro-di-Stefano-3.local>
2023-11-27 17:41:19 +0100

    fixed bug and formatted document

--- src/actRoute_generator.py
+++ src/actRoute_generator.py
@@ -9,6 +9,7 @@ from tools.parameters import Parameters as params
 
 act_route_counter = 0
 
+
 def getDrivers() -> List[Driver]:
     # reading drivers from json
     with open("./data/" + params.DRIVERS_FILENAME, "r") as driversFile:
@@ -32,6 +33,7 @@ def getDrivers() -> List[Driver]:
 
     return drivers
 
+
 def getStdRoutes() -> List[StdRoute]:
     # reading sRoutes from json
     with open("./data/" + params.SROUTES_FILENAME, "r") as driversFile:
@@ -52,6 +54,7 @@ def getStdRoutes() -> List[StdRoute]:
 
     return stdRoutes
 
+
 def generateActualRoute(std_route: StdRoute, driver: Driver) -> StdRoute:
     global act_route_counter
     # Create actual route
@@ -67,33 +70,39 @@ def generateActualRoute(std_route: StdRoute, driver: Driver) -> StdRoute:
         actualTrip = {}
 
         # Cities (NEED TO CHECK)
-        if (np.random.randint(0, 101) <= driver.citiesCrazyness):
-            #if the city is liked
-            if (stdTrip._from in driver.likedCities):
+        if np.random.randint(0, 101) <= driver.citiesCrazyness:
+            # if the city is liked
+            if stdTrip._from in driver.likedCities:
                 actualTrip.update({"from": stdTrip._from})
-            #it's the same if the city is disliked or not
+            # it's the same if the city is disliked or not
             else:
                 new_city = np.random.choice(driver.cities)
-                while (new_city == stdTrip._from or new_city == stdTrip.to or new_city in driver.dislikedCities):
+                while (
+                    new_city == stdTrip._from
+                    or new_city == stdTrip.to
+                    or new_city in driver.dislikedCities
+                ):
                     new_city = np.random.choice(driver.cities)
                 new_city = np.random.choice(driver.cities)
                 actualTrip.update({"from": new_city})
-                std_route.route[i]._from = new_city
+                std_route.route[i - 1]._from = new_city
         else:
             actualTrip.update({"from": stdTrip._from})
 
-        # Products (NEED TO CHECK) 
-        if (np.random.randint(0, 101) <= driver.productsCrazyness):
-            #if the product is liked
-            if (stdTrip.merchandise in driver.likedProducts):
+        # Products (NEED TO CHECK)
+        if np.random.randint(0, 101) <= driver.productsCrazyness:
+            # if the product is liked
+            if stdTrip.merchandise in driver.likedProducts:
                 actualTrip.update({"merchandise": stdTrip.merchandise})
-            #it's the same if the product is disliked or not
+            # it's the same if the product is disliked or not
             else:
                 new_product = np.random.choice(driver.products)
-                while (new_product == stdTrip.merchandise or new_product in driver.dislikedProducts):
+                while (
+                    new_product == stdTrip.merchandise
+                    or new_product in driver.dislikedProducts
+                ):
                     new_product = np.random.choice(driver.products)
                 actualTrip.update({"merchandise": new_product})
-                std_route.route[i].merchandise = new_product
         else:
             actualTrip.update({"merchandise": stdTrip.merchandise})
 
@@ -101,19 +110,22 @@ def generateActualRoute(std_route: StdRoute, driver: Driver) -> StdRoute:
 
     return actualRoute
 
-def actRoute_generator():
 
+def actRoute_generator():
     # Create objects from the data
     drivers: List[Driver] = getDrivers()
     stdRoutes: List[StdRoute] = getStdRoutes()
 
     for driver in drivers:
-        DRIVER_ROUTES = np.random.randint(params.MIN_ROUTES_TO_DRIVERS, params.MAX_ROUTES_TO_DRIVERS + 1)
+        DRIVER_ROUTES = np.random.randint(
+            params.MIN_ROUTES_TO_DRIVERS, params.MAX_ROUTES_TO_DRIVERS + 1
+        )
         for _ in range(DRIVER_ROUTES):
             selected_route: StdRoute = np.random.choice(stdRoutes)
             # Create actual route
             actualRoute = generateActualRoute(selected_route, driver)
-            print(actualRoute)
+            # print(actualRoute)
+
 
 if __name__ == "__main__":
     actRoute_generator()
