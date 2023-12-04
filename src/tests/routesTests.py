5351f020b7d15b3a9992d31768be562e23fb7bd8
StefanoDalMas <stefano.dalmas01@gmail.com>
2023-12-04 09:52:24 +0100

    Testing stdRoutes and cleaned test cases

--- src/tests/routesTests.py
+++ src/tests/routesTests.py
@@ -3,21 +3,60 @@ import json
 import os
 from pprint import pprint
 from typing import List
+
+from tools.parameters import Parameters as params
 from actRoute_generator import actRoute_generator
+from stdRoute_generator import stdRoute_generator
 from classes.ActRoute import ActRoute 
+from classes.StdRoute import StdRoute
 
 class TestRoutesGenerator(unittest.TestCase):
     actual_route: List[ActRoute] 
-    standard_route: List[ActRoute] 
+    standard_route: List[StdRoute]
     
     def setUp(self):
+        self.params = params
         self.actual_routes = actRoute_generator()
+        self.standard_routes = stdRoute_generator(params.ENTRIES, params.MINTRIP, params.MAXTRIP, params.MINPRODUCTS, params.MAXPRODUCTS, params.SROUTES_FILENAME)
 
     def tearDown(self):
         pass
 
-    def testActToFromLinked(self):
-        print("Testing if all routes are linked")
+
+    def testStdFromToSameNode(self):
+        print("Testing if there are no minimal loops in the standard routes")
+        for route in self.standard_routes:
+            trips = route.get("route")
+            if trips == None:
+                assert(1==0)
+            for trip in trips:
+                assert(trip.get("from")!=trip.get("to"))
+        assert(True==True)
+
+    def testActFromToSameNode(self):
+        print("Testing if there are no minimal loops in the actual routes")
+        for route in self.actual_routes:
+            trips = route.get("route")
+            if trips == None:
+                assert(1==0)
+            for trip in trips:
+                assert(trip.get("from")!=trip.get("to"))
+        assert(True==True)
+
+    def testStdFromToLinked(self):
+        print("Testing if all STANDARD routes are linked")
+        for route in self.standard_routes:
+            last = None
+            trips = route.get("route")
+            if trips == None:
+                assert(1==0)
+            for trip in trips:
+                if last != None:
+                    assert(trip.get("from") == last) 
+                last = trip.get("to")
+
+    def testActFromToLinked(self):
+        print("Testing if all ACTUAL routes are linked")
         #checks if from and to are right
         for route in self.actual_routes:
             last = None
@@ -29,15 +68,6 @@ class TestRoutesGenerator(unittest.TestCase):
                     assert(trip.get("from") == last) 
                 last = trip.get("to")
 
-    def testActFromToSameNode(self):
-        print("Testing if there are no minimal loops")
-        for route in self.actual_routes:
-            trips = route.get("route")
-            if trips == None:
-                assert(1==0)
-            for trip in trips:
-                assert(trip.get("from")!=trip.get("to"))
-        assert(True==True)
 
 if __name__ == "__main__":
     unittest.main()
