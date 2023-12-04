f72b9cf5a5abffb429ac21db2e9f01783c14c32a
StefanoDalMas <stefano.dalmas01@gmail.com>
2023-12-04 09:17:08 +0100

    Test minimal loops

--- src/tests/routesTests.py
+++ src/tests/routesTests.py
@@ -7,17 +7,19 @@ from actRoute_generator import actRoute_generator
 from classes.ActRoute import ActRoute 
 
 class TestRoutesGenerator(unittest.TestCase):
+    actual_route: List[ActRoute] 
+    standard_route: List[ActRoute] 
     
     def setUp(self):
-        pass
+        self.actual_routes = actRoute_generator()
 
     def tearDown(self):
         pass
 
-    def testStdRoutes(self):
+    def testActToFromLinked(self):
+        print("Testing if all routes are linked")
         #checks if from and to are right
-        actual_routes : List[ActRoute] = actRoute_generator()
-        for route in actual_routes:
+        for route in self.actual_routes:
             last = None
             trips = route.get("route")
             if trips == None:
@@ -26,6 +28,16 @@ class TestRoutesGenerator(unittest.TestCase):
                 if last != None:
                     assert(trip.get("from") == last) 
                 last = trip.get("to")
+
+    def testActFromToSameNode(self):
+        print("Testing if there are no minimal loops")
+        for route in self.actual_routes:
+            trips = route.get("route")
+            if trips == None:
+                assert(1==0)
+            for trip in trips:
+                assert(trip.get("from")!=trip.get("to"))
+        assert(True==True)
 
 if __name__ == "__main__":
     unittest.main()
