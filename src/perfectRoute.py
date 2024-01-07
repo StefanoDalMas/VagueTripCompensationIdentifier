353bbad44fe614d92d4f77d4db3d571e22b4d50b
StefanoDalMas <stefano.dalmas01@gmail.com>
2024-01-07 15:52:56 +0100

    Fixed types, get favorite cities from (dict, threshold)

--- src/perfectRoute.py
+++ src/perfectRoute.py
@@ -14,7 +14,6 @@ from actRoute_generator import getStdRoutes, getActRoutes
 # funzione che fa la differenza tra due oggetti descritti sopra
 
 
-
 # volgiamo creare una struttura dati che sia così
 # {driver: List[ActRoute]}
 def get_driver_actuals() -> params.driverActuals:
@@ -36,9 +35,12 @@ def get_driver_actuals() -> params.driverActuals:
 
 # vogliamo creare questo
 #  {driver: {città: quantità}}
-def calculate_liked_cities(driver_actuals: params.driverActuals) -> params.driverCalLikedCities:
+#fix return types!!!
+def calculate_liked_cities(
+    driver_actuals: params.driverActuals,
+) -> params.driverCalLikedCities:
     std_routes: List[StdRoute] = getStdRoutes()
-    driver_liked_cities: Dict[str, Dict[str, int]] = {}
+    driver_liked_cities: params.driverCalLikedCities = {}
 
     actual_dict: Dict[str, int] = {}
     standard_dict: Dict[str, int] = {}
@@ -53,8 +55,8 @@ def calculate_liked_cities(driver_actuals: params.driverActuals) -> params.drive
                     value = actual_dict.get(trip._from)
                     value += 1
 
-                actual_dict.update({trip._from: value })
- 
+                actual_dict.update({trip._from: value})
+
             # contiamo le città della standard associata
             std_id = actual.sRoute_id
             for standard in std_routes:
@@ -65,22 +67,22 @@ def calculate_liked_cities(driver_actuals: params.driverActuals) -> params.drive
                             value = standard_dict.get(trip._from)
                             value += 1
 
-                        standard_dict.update({trip._from: value })
+                        standard_dict.update({trip._from: value})
 
-           
             # facciamo sottrazione tra i dizionari e la aggiungiamo al totale
-            calculated_liked_cities = update_dict(calculated_liked_cities, dict_difference(actual_dict, standard_dict))
-
-
-
+            calculated_liked_cities = update_dict(
+                calculated_liked_cities, dict_difference(actual_dict, standard_dict)
+            )
 
         # crea threshold per decidere se città è buona o meno
         max_value = max(calculated_liked_cities.values())
         threshold = int(max_value * params.THRESHOLD_MOLTIPLICATOR)
 
+        calc_liked_cities_thr_tuple: Tuple[Dict[str, int], int] = (
+            calculated_liked_cities,
+            threshold,
+        )
 
-        calc_liked_cities_thr_tuple: Tuple[Dict[str, int], int] = (calculated_liked_cities, threshold)
-            
         driver_liked_cities.update({driver: calc_liked_cities_thr_tuple})
 
         threshold = 0
@@ -89,10 +91,11 @@ def calculate_liked_cities(driver_actuals: params.driverActuals) -> params.drive
         calculated_liked_cities = {}
         calc_liked_cities_thr_tuple = {}
 
-    return driver_liked_cities
+    return get_favourite_cities_dict(driver_liked_cities)
+
 
 def dict_difference(dict1: Dict[str, int], dict2: Dict[str, int]) -> Dict[str, int]:
-    result = {}
+    result : Dict[str,int] = {}
     for item in dict1:
         if item in dict2:
             if dict1.get(item) - dict2.get(item) > 0:
@@ -100,22 +103,37 @@ def dict_difference(dict1: Dict[str, int], dict2: Dict[str, int]) -> Dict[str, i
 
     return result
 
+
 def update_dict(dict1: Dict[str, int], dict2: Dict[str, int]) -> Dict[str, int]:
     if dict1 == None and dict2 == None:
         return {}
     else:
-        if (dict1 == None):
+        if dict1 == None:
             return dict2
-        if (dict2 == None):
+        if dict2 == None:
             return dict1
-        
+
         keys_set = set(dict1) | set(dict2)
         for key in keys_set:
             dict1.update({key: (dict1.get(key, 0) + dict2.get(key, 0))})
         return dict1
 
+def get_favourite_cities_dict(dict:params.driverCalLikedCities) -> Dict[str,List[str]]:
+    res_dict:Dict[str,List[str]] = {}
+    for driver, tuple in dict.items():
+        res_list : List[str] = []
+        for city, occurrencies in tuple[0].items():
+            if occurrencies > tuple[1]:
+                res_list.append(city)
+        res_dict.update({driver:res_list})
+    return res_dict
+
 
 if __name__ == "__main__":
     # dict[0] -> dictionary, dict[1] -> threshold
-    dict = calculate_liked_cities(get_driver_actuals())
+    fav_cities : Dict[str,List[str]] = calculate_liked_cities(get_driver_actuals())
+
+    #next step : get best merchandise for each driver
+
+    #next step : generate perfect route using fav_cities and fav_merchandise
     print("stop")
